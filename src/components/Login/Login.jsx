a5a9211c0de29b81d0d5282768c8aada664523e4
phlabib <ph.labib@gmail.com>
2023-09-28 17:06:27 +0600

    Error (auth/operation-not-allowed).

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import app from '../../Firebase.init';
 
 const Login = () => {
-    const auth = getAuth (app)
+    const auth = getAuth (app);
+    console.log(app)
     const provider = new GoogleAuthProvider();
 
     const handleGoogleSignIn = () => {
-        console.log('google mama is coming')
+        signInWithPopup(auth, provider) 
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(error => {
+            console.log('error', error.message)
+        })
     }
     return (
         <div>
