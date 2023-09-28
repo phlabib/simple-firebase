c61f2910f8169e5496267c66cd714b038abfad15
phlabib <ph.labib@gmail.com>
2023-09-28 22:27:28 +0600

    49-6 Enable Github sign in and Create github application

--- src/_stapes.js
+++ src/_stapes.js
@@ -1,3 +1,7 @@
+/*                      ------------------------
+                        INATALL SETUP
+                        ------------------------
+*/
 // 1. visti : console.firebase.google.com
 // 2. create project (skip google analytics)
 // 3. Register app (create config)
@@ -5,11 +9,33 @@
 // 5. add config file to your project 
 // 6. DANGER : Don't publish or make firebase config 
 // to public by pushing those to github.
+
+                /* 
+                ------------------------------
+                INTEGRATION
+                -----------------------
+                */
 // 7. Go to Docs > Build > Authentication > Web > Get Started
 // 8. export app from the firebase.config.js file : export default app
 // 9. Login.jsx : import getAuth from firebase/auth
 // 10. create const auth = getAuth(app)
+
+                /* 
+                ----------------------------
+                PROVIDER SETUP
+                -------------------------------
+                */
 // 11. import googleAuthProvider and create a new provider 
 // 12. use signInWithPopup and pass auth and provide
 // 13. activate sign-in method (google, facebook, github, etc)
 // 14. [vite]: change 127.0.0.1 to localhost
+/* 
+
+/* 
+-------------------------
+More Auth Provider
+-----------------------------
+1.  activate the auth provier (creat app, priver redirect url, client id, client 
+    secret )
+*/
+
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,48 +1,77 @@
-import React, { useState } from 'react';
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
-import app from '../../Firebase.init';
+import React, { useState } from "react";
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import app from "../../Firebase.init";
 
 const Login = () => {
-    const [user, setUser] = useState ();
-    const auth = getAuth (app);
-    console.log(app);
-    const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState();
+  const auth = getAuth(app);
+  console.log(app);
+  const googleprovider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
 
-    const handleGoogleSignIn = () => {
-        signInWithPopup(auth, provider) 
-        .then(result => {
-            const loggedInUser = result.user;
-            console.log(loggedInUser);
-            setUser(loggedInUser);
-        })
-        .catch(error => {
-            console.log('error', error.message)
-        })
-    }
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleprovider)
+      .then((result) => {
+        const loggedInUser = result.user;
+        console.log(loggedInUser);
+        setUser(loggedInUser);
+      })
+      .catch((error) => {
+        console.log("error", error.message);
+      });
+  };
 
-    const handleSignOut = () => {
-        signOut(auth)
-        .then(result => {
-            console.log(result);
-            setUser(null) 
-        } )
-        .catch(error => {
-            console.log(error);
-        })
-    }
-    return (
+  const handleGitHubSignIn = () => {
+    signInWithPopup (auth, githubProvider)
+    .then(result => {
+        const loggedUser = result.user
+        console.log(loggedUser);
+        setUser(loggedUser)
+    })
+    
+    .catch(error =>{
+        console.log(error);
+    } )
+  }
+
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then((result) => {
+        console.log(result);
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div>
+      {user ? 
+        <button onClick={handleSignOut}>Sign out</button>
+      : 
         <div>
-           {  user?
-           <button onClick={handleSignOut}>Sign out</button> :
-            <button onClick={handleGoogleSignIn}>Google LogIn</button>}
-            {user && <div>
-            <h3>User : {user.displayName}</h3>
-            <p>Email :{user.email} </p>
-            <img src={user.photoURL} alt="" />
-            <p>UserId : {user.uid}</p>
-            </div>}
+          <button onClick={handleGoogleSignIn}>Google LogIn</button>
+          <button onClick={handleGitHubSignIn}>GitHub Login</button>
         </div>
-    );
+      }
+
+      {user && (
+        <div>
+          <h3>User : {user.displayName}</h3>
+          <p>Email :{user.email} </p>
+          <img src={user.photoURL} alt="" />
+          <p>UserId : {user.uid}</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Login;
